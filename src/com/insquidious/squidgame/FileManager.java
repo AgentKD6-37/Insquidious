8dbf12202a4c41cc4e427cf6c10bc6c1c462dd6f
Michael Stack <mstack000@gmail.com>
2021-11-05 17:55:33 -0400

    Added assets, built out GameEngine.java, MenuManager.java, and FileManager.java.
    Save editor is BROKEN and needs to be fixed. I will create a ticket for this.

--- src/com/insquidious/squidgame/FileManager.java
+++ src/com/insquidious/squidgame/FileManager.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Properties;
+import java.util.Scanner;
 
 /*
  *File manager can import, read, write, or create files
@@ -14,41 +16,47 @@ class FileManager {
     private FileInputStream in = null;
     private FileOutputStream out = null;
     private FileWriter writer;
+    private Scanner scanner = new Scanner(System.in);
     private FileReader reader;
-    private String saveGame = "Assets/save-game.txt";
-    public File save;
-    public File openingMenu;
+    private String saveGame = "save-game.properties";
+    public Properties save;
 
     //returns the save file to the game engine to be read
-    public void getSaveFile() throws IOException {
-        this.save = new File(saveGame);
-        if (save.exists()){
-            in = new FileInputStream(String.valueOf(Path.of(saveGame)));
-            in.close();
-        }else{
-            System.out.println("No save file exists for this player");
-        }
+    public void getSaveFile(Properties flag) throws IOException {
+        in = new FileInputStream(saveGame);
     }
 
-    //creates and writes to a new save file
-    public void setSaveFile(String name) throws IOException {
-        this.save = new File(saveGame);
-        out = new FileOutputStream(String.valueOf(Path.of(saveGame)));
-        writer = new FileWriter(save);
-        writer.write(name);
-        writer.close();
-        out.close();
-    }
-
-    public void getOpeningMenuArt() throws IOException{
-        String openingMenuArt = "Assets/opening-menu-art.txt";
-        String openingMenuBanner = "Assets/opening-menu-banner.txt";
+    public void getArtFile(String fileName) throws IOException{
+        String art = "Assets/" + fileName;
         var out = new BufferedOutputStream(System.out);
-        Files.copy(Path.of(openingMenuArt), out);
-        Files.copy(Path.of(openingMenuBanner), out);
+        Files.copy(Path.of(art), out);
         out.flush();
     }
 
 
+    //save-game.properties is set by default to null/0 values and then overwritten by the newPlayerCreator
+    void newPlayerCreator() throws IOException {
+        in = new FileInputStream(saveGame);
+        save.load(in);
+        out = new FileOutputStream(saveGame);
+        System.out.print("What is your name? : ");
+        String name = scanner.next();
+        if(name != null){
+            save.put("playerName", name );
+        }else{
+            System.out.println("Name cannot be empty!");
+            name = scanner.next();
+        }
+        System.out.print("Have you played this game before? [Y/N] : ");
+        String newPlayer = scanner.next();
+        if (newPlayer.equals("N")) {
+            save.put("newPlayer", false);
+        } else {
+            System.out.println("Enter only Y or N");
+            newPlayer = scanner.next();
+        }
+        in.close();
+        out.close();
+    }
 
 }
--- src/com/insquidious/squidgame/MenuManager.java
+++ src/com/insquidious/squidgame/MenuManager.java
@@ -1,44 +1,36 @@
 package com.insquidious.squidgame;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
+
+/*
+ * Class passes a set of art assets to the engine
+ */
 
 public class MenuManager {
     Scanner playerInput = new Scanner(System.in);
     FileManager fileManager = new FileManager();
 
-    public void openingMenu() throws IOException {
+    public void MenuFiles() throws IOException {
 
-        fileManager.getOpeningMenuArt();
-        System.out.println("");
-        System.out.println("Welcome. Would you like to Play?");
-        System.out.println("1 - New Story Game");
-        System.out.println("2 - Mini Game Selector");
-        System.out.println("3 - Exit");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Please select a number to continue");
-        int choice = playerInput.nextInt();
-        while (choice != 3) {
-
-            if (choice < 1 || choice > 3) {
-
-                System.out.println("Enter \"1\", \"2\", or \"3\"");
-                choice = playerInput.nextInt();
-
-            } else if (choice == 1) {
-                System.out.println("What is your name?");
-                String name = playerInput.next();
-                fileManager.setSaveFile(name);
-            } else if (choice == 2) {
-                dormMenu();
-            } else if (choice == 3) {
-                System.exit(0);
-            }
-
-        }
+        fileManager.getArtFile("opening-menu-art.txt");
+        fileManager.getArtFile("opening-menu-banner.txt");
+        fileManager.getArtFile("opening-menu-dialogue.txt");
     }
 
-    private void dormMenu() {
+     void dormMenuFiles() throws IOException {
+        fileManager.getArtFile("dorm-menu.txt");
+        fileManager.getArtFile("dorm-menu-dialogue.txt");
+    }
+
+    void eliminatedMenuFiles() throws IOException{
+        fileManager.getArtFile("eliminated-menu-art.txt");
+        fileManager.getArtFile("eliminated-menu-dialogue");
+    }
+
+    void winnerMenuFiles() throws IOException{
+        fileManager.getArtFile("winner-menu-art.txt");
+        fileManager.getArtFile("winner-menu-dialogue");
     }
 }
