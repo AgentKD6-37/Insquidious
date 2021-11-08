6fff6bd9c7e39b76b3690ad54695e5c43f1ee52e
Justin Hammel <justinhammel62@gmail.com>
2021-11-08 09:10:53 -0800

    Dice roller version 1.1 BoardFactory and RLGL Initial board

--- src/com/insquidious/squidgame/board/RedLightGreenLight.java
+++ src/com/insquidious/squidgame/board/RedLightGreenLight.java
@@ -3,16 +3,19 @@ package com.insquidious.squidgame.board;
 //concrete implementation of BoardType
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RedLightGreenLight extends Board {
     //static fields
     public static final Integer LENGTH = 100;
     public static final Integer WIDTH = 20;
     public static final Integer MAX_PLAYERS = 20;
+    //TODO: Time synchronization or implement time countdown
+    public static final Integer TIME_LIMIT = 120_000;
 
     //fields
-    private static ArrayList<Integer> playerList= new ArrayList<Integer>();
-    private static ArrayList<Integer> rows= new ArrayList<Integer>();
+    private static List<Integer> playerList= new ArrayList<Integer>();
+    private static List<Integer> rows= new ArrayList<Integer>();
 
     //Constructors
     RedLightGreenLight() {
@@ -21,15 +24,29 @@ public class RedLightGreenLight extends Board {
     }
 
     //Methods
+
+    /*
+    * Method moves Player objects across the board
+    * TODO: need to coordinate with Zrybea about controlling the speed of the player across
+    *  the board, pass speed? convert speed to movement? I also need a way to track each players current row.
+    */
+    public void movePlayer() {
+        int moveForward;
+        while (true){
+
+        }
+    }
+
+    /*
+     * This initializes the 2d array that represents the game board.
+     * The rows are the outer ArrayList. Rows will start at 0.
+     * Max number of rows determined by LENGTH variable.
+     */
     @Override
     protected void initBoard() {
         //test method
         System.out.println("Creating red light, green light board!");
 
-        /*
-        * This initializes the 2d array that represents the game board.
-        * The rows are the outer ArrayList. Rows will start at 0.
-        */
         ArrayList<ArrayList<Integer>> boardGrid =new ArrayList<ArrayList<Integer> >();
 
         //adds the first "row" to the board starting at 0
@@ -43,91 +60,13 @@ public class RedLightGreenLight extends Board {
             playerList.add(i);
         }
 
-        //this will place the players on the board and set up the first row in the board
-
-
         //creating rows for the outer array list
-        for (int i = 0; i < WIDTH; i++) {
-
-        }
-        boardGrid.add(playerList);
-    }
-
-
-    //TODO:Danger! There be dragons below here.
-    /*//static fields
-    private static final double TIME_LIMIT = 120000;
-
-
-    private List<Integer> players = new ArrayList();    //This would be shifted to a List<Players>
-
-    private int[] initPosition = {0, 0};    //Players have a field to contain their location, or board keeps positions by ID
-    private Integer initPlayerID = 0;       //these values are only used to "wipe" the board when started
-
-    private Map<int[], Integer> board = new HashMap<>();  //would like for the board to be <position, Player obj>
-
-    //constructor
-    public RedLightGreenLight() {
-        setLength(length);
-        setWidth(width);
-        initBoard();
-        //fillPlayerList();
-        //addPlayers();
-    }
-
-    //methods
-    public void initBoard() {
-        //this zeros out all board positions
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < length; j++) {
-                //Thinking player ID should be replaced with player obj, or could just pull id from player obj
-                board.put(initPosition, initPlayerID);
-            }
+        //TODO: Will need to swap Integer for Player
+        for (Integer player: playerList) {
+            //adding players to the starting row 0
+            boardGrid.get(0).add(player);
         }
     }
 
-    //using this as dummy holder for player objects
-    public void fillPlayerList() {
-        for (int i = 1; i < 21; i++) {
-            players.add(Integer.valueOf(i));
-        }
-    }
 
-    //adds players to their starting points on the map
-    public void addPlayers() {
-        int[] startPosition = {1, 1};
-        for (Integer playerID : players) {
-            board.put(startPosition , playerID);
-            startPosition[0]++;
-        }
-    }
-
-    //accessors
-    public int getLength() {
-        return length;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    @Override
-    public String toString() {
-        return "RedLightGreenLight{" +
-                "length=" + length +
-                ", width=" + width +
-                ", players=" + players +
-                ", initPosition=" + Arrays.toString(initPosition) +
-                ", initPlayerID=" + initPlayerID +
-                ", board=" + board +
-                '}';
-    }*/
 }
